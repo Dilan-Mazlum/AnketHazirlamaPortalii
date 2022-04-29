97c2091364c27c67d34948d4d49d0233b59dcac8
Dilan Mazlum <dilanmazlum513@gmail.com>
2022-04-29 22:14:21 +0300

    Add project files.

---
+++ AnketHazirlamaPortali/ViewModel/CevapModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnketHazirlamaPortali.ViewModel
+{
+    public class CevapModel
+    {
+        public int CevapId { get; set; }
+        public Nullable<int> KullaniciCevapId { get; set; }
+        public Nullable<int> SecilenCevapId { get; set; }
+
+    }
+}
